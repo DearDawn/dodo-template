41455754448f93dbe1611875325f8aace8684b1f
tangjingchun <tangjingchun@bytedance.com>
2022-05-09 19:00:39 +0800

    feat: build.sh

--- react-multi-page-app/build.sh
+++ react-multi-page-app/build.sh
@@ -2,6 +2,7 @@
 
 PROJECT_NAME=$1
 PROJECT_PATH=$2
+DIST_PATH='./dist'
 
 if [[ -z $PROJECT_NAME || ! -d $PROJECT_PATH ]]; then
   echo "params empty"
@@ -27,8 +28,8 @@ npm install
 echo "start build"
 npm run build
 
-if [[ $? != 0 || ! -d './build' ]]; then
-  echo "build failed, dir empty"
+if [[ $? != 0 || ! -d $DIST_PATH ]]; then
+  echo "build failed, dist dir empty"
   exit 1
 fi
 
@@ -39,4 +40,4 @@ if [[ -d $PROJECT_PATH && $PROJECT_NAME ]]; then
   rm -rf $PUBLIC_PATH
 fi
 
-mv ./dist $PROJECT_PATH$PROJECT_NAME
+mv $DIST_PATH $PROJECT_PATH$PROJECT_NAME
