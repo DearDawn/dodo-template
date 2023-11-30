6ae78bf9099cf7747441507c0563de99da9a7ea9
tangqiuqiu <tangqiuqiu@pinduoduo.com>
2023-11-30 17:52:35 +0800

    feat: update react template

--- react-multi-page-app/src/pages/home/App.tsx
+++ react-multi-page-app/src/pages/home/App.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import * as styles from './App.module.less'
+import * as React from 'react';
+import * as styles from './App.module.less';
 
 interface IProps {}
 
 export const App = (props: IProps) => {
   React.useEffect(() => {
-    console.log('[dodo] ', 'home')
-  }, [])
+    console.log('[dodo] ', 'home');
+  }, []);
 
-  return <div className={styles.app}>Home</div>
-}
+  return <div className={styles.app}>Home</div>;
+};
