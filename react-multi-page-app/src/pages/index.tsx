1c40fab04bb4eefb53731abd344f9fb80ef426f7
tangqiuqiu <tangqiuqiu@pinduoduo.com>
2023-12-06 17:13:49 +0800

    feat: 更新模板

--- react-multi-page-app/src/pages/index.tsx
+++ react-multi-page-app/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-const app = document.getElementById('app')
-ReactDOM.render(<App />, app)
+const app = document.getElementById('app');
+const root = createRoot(app);
+root.render(<App />);
